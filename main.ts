fb1a058c74a7dbd606f8d3e7accf1805c8e559e8
samuelcavadia <74431257+samuelcavadia@users.noreply.github.com>
2020-11-13 15:29:09 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,123 @@
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . # # #
+        . # . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . # #
+        . . # . #
+        . # . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # . #
+        . # . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # #
+        . . # . #
+        . # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # #
+        . . # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # #
+        . . # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # #
+        . . # . #
+        . # # # #
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # . #
+        . # . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . # #
+        . . # . #
+        . # . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . # # #
+        . # . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . # # # #
+        # # . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+})
